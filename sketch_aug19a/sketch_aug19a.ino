dfd61e8849d3099b57c6cf79955592fd341f7656
ARSHSUU <133488771+ARSHSUU@users.noreply.github.com>
2023-08-19 17:13:16 +0530

    Add files via upload

---
+++ sketch_aug19a/sketch_aug19a.ino
@@ -0,0 +1,72 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+// Morese code for name:ARSH      
+  //Blinking A in morse code 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);             
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);
+    //Blinking R in morse code  
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);    
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);         
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(1000);  
+  //Blinking S in morse code
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);    
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000); 
+  //Blinking H in morse code 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000);  
+  //name printed      
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(10000);    
+}
